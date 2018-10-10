783bc1208fa1a0b2a855f0a2c335daeff713f8fa
Slava Avdeev <34892700+ProMindSl@users.noreply.github.com>
2018-10-10 13:46:07 +0300

    add animation in front

--- src/blocks/front/front.js
+++ src/blocks/front/front.js
@@ -13,6 +13,7 @@ function bgMove() /* start position of bg is -80px*/
 {
 	let scrolled = window.pageYOffset || document.documentElement.scrollTop;
 	let bg = document.getElementById("bg_front");
-	/*let curPosY = getNumFromBgPosString(bg.style.backgroundPositionY);*/
-	bg.style.backgroundPositionY = (-70+(scrolled/2))+'px';
+	/*bg.style.backgroundPositionY = (-70+(scrolled/2))+'px';*/
+	let posY = (scrolled/10)-70+'px';
+	TweenMax.to(bg, 1, {backgroundPositionY:posY});
 };
