177f1227eaec7fdb11f38ef5b80488c9338111f3
Slava Avdeev <34892700+ProMindSl@users.noreply.github.com>
2018-10-12 16:21:19 +0300

    some fix for picture animation

--- src/blocks/picture/picture.js
+++ src/blocks/picture/picture.js
@@ -11,12 +11,44 @@ $('.picture').fancybox({
 var tl = new TimelineMax();
 var picCount = 6; /* Кол-во картинок в галерее */ 
 
-for(let i=0; i<picCount; i++)
+/*for(let i=0; i<picCount; i++)
 {
-	tl.from('#g_pic'+(i+1), 0.5, { y:400, 
+	let dely = (i==0)? 0 : -0.17;
+	tl.from('#g_pic'+(i+1), 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: (i==0)? 0 : -0.25
+									})
+}*/
+
+tl.from('#g_pic1', 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: 0
+									})
+   .from('#g_pic2', 0.44, { y:400, 
 								    opacity:0, 
 								    ease: Power4.easeOut,
 								    delay: -0.25
 									})
-}
+   .from('#g_pic3', 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: -0.25
+									})
+   .from('#g_pic4', 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: -0.25
+									})
+   .from('#g_pic5', 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: -0.25
+									})
+   .from('#g_pic6', 0.44, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: -0.25
+									});
 
