d67cceb379a7bd4e94732935c1ce186320e84885
Slava Avdeev <34892700+ProMindSl@users.noreply.github.com>
2018-10-12 16:02:08 +0300

    animate galary

--- src/blocks/picture/picture.js
+++ src/blocks/picture/picture.js
@@ -6,3 +6,17 @@ $('.picture').fancybox({
     'close'
   ]
 });
+
+/* Запуск анимации картинок галереи */
+var tl = new TimelineMax();
+var picCount = 6; /* Кол-во картинок в галерее */ 
+
+for(let i=0; i<picCount; i++)
+{
+	tl.from('#g_pic'+(i+1), 0.5, { y:400, 
+								    opacity:0, 
+								    ease: Power4.easeOut,
+								    delay: -0.25
+									})
+}
+
